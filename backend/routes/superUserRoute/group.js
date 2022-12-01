6bbc57ceab3baa8e6bb894d5118705c2c3afe998
Luke Ren <luke.ren@mail.utoronto.ca>
2022-11-30 20:50:25 -0500

    delete group done

--- backend/routes/superUserRoute/group.js
+++ backend/routes/superUserRoute/group.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const {createGroup, superuserFetchGroup, superuserUpdateGroup} = require("../../services/groups/group");
+const {
+  createGroup,
+  superuserFetchGroup,
+  superuserUpdateGroup,
+  superuserDeleteGroup
+} = require("../../services/groups/group");
 
 
 const router = express.Router({mergeParams: true});
 
 router.post('/', createGroup);
-router.get('/:id/', superuserFetchGroup)
-router.put('/:id/', superuserUpdateGroup)
+router.get('/:id/', superuserFetchGroup);
+router.put('/:id/', superuserUpdateGroup);
+router.delete('/:id', superuserDeleteGroup);
 
 module.exports = router;
--- backend/services/groups/group.js
+++ backend/services/groups/group.js
@@ -4,9 +4,9 @@ const createGroup = async (req, res, next) => {
     const form = req.body;
     if (!form || !form.label)
       return res.status(400).json({success: false, message: 'Wrong information given'});
-    const group = GDBGroupModel(form)
-    await group.save()
-    return res.status(200).json({success: true})
+    const group = GDBGroupModel(form);
+    await group.save();
+    return res.status(200).json({success: true});
   } catch (e) {
     next(e);
   }
@@ -14,28 +14,36 @@ const createGroup = async (req, res, next) => {
 
 const superuserFetchGroup = async (req, res, next) => {
   try {
-    const {id} =req.params
-    if(!id)
+    const {id} = req.params;
+    if (!id)
       return res.status(400).json({success: false, message: 'No id is given'});
     const group = await GDBGroupModel.findById(id);
-    if(!group)
+    if (!group)
       return res.status(400).json({success: false, message: 'No such group'});
-    return res.status(200).json({success: true, group: group})
-  }catch (e) {
+    return res.status(200).json({success: true, group: group});
+  } catch (e) {
     next(e);
   }
-}
+};
 const superuserUpdateGroup = async (req, res, next) => {
   try {
-    const {id} = req.params
-    const form = req.body
-    if(!id || !form)
-      return res.status(400).json({success: false, message: 'Invalid information given'});
+    const {id} = req.params;
+    const form = req.body;
+    if (!id || !form)
+      return res.status(400).json({success: false, message: 'Invalid information is given'});
     await GDBGroupModel.findByIdAndUpdate(id, form);
-    return res.status(200).json({success: true, message: 'Successfully update group '+ id})
-  }catch (e) {
+    return res.status(200).json({success: true, message: 'Successfully updated group ' + id});
+  } catch (e) {
     next(e);
   }
-}
+};
 
-module.exports = {createGroup, superuserFetchGroup, superuserUpdateGroup};
+const superuserDeleteGroup = async (req, res, next) => {
+  const {id} = req.params;
+  if (!id)
+    return res.status(400).json({success: false, message: 'Invalid information is given'});
+  await GDBGroupModel.findByIdAndDelete(id);
+  return res.status(200).json({success: true, message: 'Successfully deleted group ' + id});
+};
+
+module.exports = {createGroup, superuserFetchGroup, superuserUpdateGroup, superuserDeleteGroup};

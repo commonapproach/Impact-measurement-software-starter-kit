01bdc8ea4d0aa3f33d0c1576139b1f4db348fd1d
Luke Ren <luke.ren@mail.utoronto.ca>
2022-12-15 19:44:34 -0500

    fetching groups for groupAdmin, done

--- backend/services/groups/groups.js
+++ backend/services/groups/groups.js
@@ -1,16 +1,22 @@
 const {GDBGroupModel} = require("../../models/group");
 const superuserFetchGroups = async (req, res, next) => {
-  try{
+  try {
     const groups = await GDBGroupModel.find({});
     return res.status(200).json({groups});
-  }catch (e) {
+  } catch (e) {
     next(e);
   }
-}
+};
 
 const groupAdminFetchGroups = async (req, res, next) => {
+  try {
+    const groupAdminId = req.session._id;
+    const groups = await GDBGroupModel.find({administrator: {_id: groupAdminId}})
+    return res.status(200).json({groups})
+  } catch (e) {
+    next(e);
+  }
+};
 
-}
 
-
-module.exports = {superuserFetchGroups, groupAdminFetchGroups}
+module.exports = {superuserFetchGroups, groupAdminFetchGroups};

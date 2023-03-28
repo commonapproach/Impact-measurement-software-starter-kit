89dcb19cf74b5c2f8c71061b0a95f02e515f366d
Luke Ren <luke.ren@mail.utoronto.ca>
2023-03-28 17:11:28 -0400

    update models: link indicator with outcome

--- backend/models/indicator.js
+++ backend/models/indicator.js
@@ -1,10 +1,12 @@
 const {createGraphDBModel, Types} = require("../utils/graphdb");
 const {GDBIndicatorReportModel} = require("./indicatorReport");
 const {GDBUnitOfMeasure} = require("./measure");
+const {GDBOutcomeModel} = require("./outcome");
 
 const GDBIndicatorModel = createGraphDBModel({
   name: {type: String, internalKey: 'tove_org:hasName'},
   description: {type: String, internalKey: 'cids:hasDescription'},
+  forOutcome: {type: GDBOutcomeModel, internalKey: 'cids:forOutcome'},
   indicatorReport: {type: [GDBIndicatorReportModel], internalKey: 'cids:hasIndicatorReport'},
   forOrganization: {type: [Types.NamedIndividual], internalKey: 'cids:forOrganization'},
   unitOfMeasure: {type: GDBUnitOfMeasure, internalKey: 'iso21972:hasUnit'}

e70905ab2ea777ea0001a91fdb899227de1db4de
Luke Ren <luke.ren@mail.utoronto.ca>
2023-05-02 13:10:02 -0400

    currently disabled fileUploading system for Kate

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -53,7 +53,7 @@ function Dashboard() {
       <NavButton to={'/themes'} icon={<Edit/>} key={'themes'}
                  text="Manage Themes"/>
 
-      <NavButton to={'/fileUploading'} icon={<FileUpload/>} key={'fileUploading'}
+      <NavButton to={'/fileUploading'} icon={<FileUpload/>} key={'fileUploading'} disabled
                  text="File Upload"/>
 
 

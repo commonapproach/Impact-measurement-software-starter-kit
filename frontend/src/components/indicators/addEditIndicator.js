3578b8433c78ab49164959b9f8d145ca09909732
Luke Ren <luke.ren@mail.utoronto.ca>
2023-07-21 09:17:03 -0400

    indicator viewing page

--- frontend/src/components/indicators/addEditIndicator.js
+++ frontend/src/components/indicators/addEditIndicator.js
@@ -1,7 +1,7 @@
 import {makeStyles} from "@mui/styles";
 import {useNavigate, useParams} from "react-router-dom";
 import React, {useEffect, useState, useContext} from "react";
-import {Loading} from "../shared";
+import {Link, Loading} from "../shared";
 import {Button, Container, Paper, Typography} from "@mui/material";
 import LoadingButton from "../shared/LoadingButton";
 import {AlertDialog} from "../shared/Dialogs";
@@ -143,38 +143,58 @@ export default function AddEditIndicator() {
   if (loading)
     return <Loading/>;
 
+
   return (
     <Container maxWidth="md">
-      <Paper sx={{p: 2}} variant={'outlined'}>
-        <Typography variant={'h4'}> Indicator </Typography>
-        <IndicatorField
-          disabled={mode === 'view'}
-          disabledURI={mode !== 'new'}
-          disabledOrganization={!!orgUri}
-          defaultValue={form}
-          required
-          onChange={(state) => {
-            setForm(form => ({...form, ...state}));
-          }}
-          importErrors={errors}
-        />
+      {mode === 'view' ?
+        <Paper sx={{p: 2}} variant={'outlined'}>
 
-        {mode === 'view' ?
-          <div/> :
-          <Button variant="contained" color="primary" className={classes.button} onClick={handleSubmit}>
-            Submit
-          </Button>}
+          <Typography variant={'h6'}> {`Name:`} </Typography>
+          <Typography variant={'body1'}> {`${form.name}`} </Typography>
+            <Typography variant={'h6'}> {`URI:`} </Typography>
+          <Typography variant={'body1'}> {`${form.uri}`} </Typography>
+            <Typography variant={'h6'}> {`Organizations:`} </Typography>
+          <Typography variant={'body1'}> {form.organizations.map(orgUri => {
+            return (<Link to={`/indicator/${encodeURIComponent(orgUri)}/view`} colorWithHover color={'#2f5ac7'}>{orgUri}</Link>)
+          })} </Typography>
+            <Typography variant={'h6'}> {`Unit of Measure:`} </Typography>
+          <Typography variant={'body1'}> {`${form.unitOfMeasure}`} </Typography>
+            <Typography variant={'h6'}> {`Description:`} </Typography>
+          <Typography variant={'body1'}> {`${form.description}`} </Typography>
 
-        <AlertDialog dialogContentText={"You won't be able to edit the information after clicking CONFIRM."}
-                     dialogTitle={mode === 'new' ? 'Are you sure you want to create this new Indicator?' :
-                       'Are you sure you want to update this Indicator?'}
-                     buttons={[<Button onClick={() => setState(state => ({...state, submitDialog: false}))}
-                                       key={'cancel'}>{'cancel'}</Button>,
-                       <LoadingButton noDefaultStyle variant="text" color="primary" loading={state.loadingButton}
-                                      key={'confirm'}
-                                      onClick={handleConfirm} children="confirm" autoFocus/>]}
-                     open={state.submitDialog}/>
-      </Paper>
+        </Paper>
+        :
+        <Paper sx={{p: 2}} variant={'outlined'}>
+          <Typography variant={'h4'}> Indicator </Typography>
+          <IndicatorField
+            disabled={mode === 'view'}
+            disabledURI={mode !== 'new'}
+            disabledOrganization={!!orgUri}
+            defaultValue={form}
+            required
+            onChange={(state) => {
+              setForm(form => ({...form, ...state}));
+            }}
+            importErrors={errors}
+          />
+
+          {mode === 'view' ?
+            <div/> :
+            <Button variant="contained" color="primary" className={classes.button} onClick={handleSubmit}>
+              Submit
+            </Button>}
+
+          <AlertDialog dialogContentText={"You won't be able to edit the information after clicking CONFIRM."}
+                       dialogTitle={mode === 'new' ? 'Are you sure you want to create this new Indicator?' :
+                         'Are you sure you want to update this Indicator?'}
+                       buttons={[<Button onClick={() => setState(state => ({...state, submitDialog: false}))}
+                                         key={'cancel'}>{'cancel'}</Button>,
+                         <LoadingButton noDefaultStyle variant="text" color="primary" loading={state.loadingButton}
+                                        key={'confirm'}
+                                        onClick={handleConfirm} children="confirm" autoFocus/>]}
+                       open={state.submitDialog}/>
+        </Paper>}
+
     </Container>);
 
 }

6b032fb23eb7e2a4b0b27ed163b2dcf740652d25
Luke Ren <luke.ren@mail.utoronto.ca>
2023-08-14 17:44:58 -0400

    create code object

---
+++ backend/models/code.js
@@ -0,0 +1,20 @@
+const {createGraphDBModel, Types} = require("graphdb-utils");
+const {GDBUserAccountModel} = require("./userAccount");
+const {GDBOrganizationModel} = require("./organization");
+const {GDBMeasureModel} = require("./measure");
+
+const GDBCodeModel = createGraphDBModel({
+  definedBy: {type: GDBOrganizationModel, internalKey: 'cids:definedBy'},
+  specification: {type: String, internalKey: 'cids:hasSpecification'},
+  identifier: {type: String, internalKey: 'tove_org:hasIdentification'},
+  name: {type: String, internalKey: 'tove_org:hasName'},
+  description: {type: String, internalKey: 'schema:hasDescription'},
+  codeValue: {type: String, internalKey: 'schema:codeValue'},
+  value: {type: GDBMeasureModel, internalKey: 'iso21972:value'}
+}, {
+  rdfTypes: ['cids:Code'], name: 'code'
+});
+
+module.exports = {
+  GDBCodeModel
+}

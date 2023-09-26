9297c2db239383d1ff1eb5e655e1c947ef808b97
Luke Ren <luke.ren@mail.utoronto.ca>
2023-09-26 17:53:52 -0400

    outcome viewing page

--- backend/models/outcome.js
+++ backend/models/outcome.js
@@ -1,11 +1,10 @@
 const {createGraphDBModel, Types} = require("graphdb-utils");
-const {GDBThemeModel} = require("./theme");
 
 
 const GDBOutcomeModel = createGraphDBModel({
   name: {type: String, internalKey: 'cids:hasName'}, // todo: here is issue, on protege, it should be tov_org:hasName
   description: {type: String, internalKey: 'cids:hasDescription'},
-  themes: {type: [GDBThemeModel], internalKey: 'cids:forTheme'},
+  themes: {type: [() => require("./theme").GDBThemeModel], internalKey: 'cids:forTheme'},
   stakeholderOutcomes: {type: [() => require("./stakeholderOutcome").GDBStakeholderOutcomeModel], internalKey: 'cids:hasStakeholderOutcome'},
   forOrganization: {type: () => require("./organization").GDBOrganizationModel, internalKey: 'cids:forOrganization'},
   indicators: {type:　[() => require("./indicator").GDBIndicatorModel], internalKey: 'cids:hasIndicator'},
